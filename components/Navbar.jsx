8dc120d464f07734ad6c236c4e57980163a788af
Diganta Singh Panwar <114343344+digantaa@users.noreply.github.com>
2025-10-24 12:01:56 +0530

    supressing unnecessary hydration warning

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,19 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 export default function Navbar() {
     return (
-        <nav className="bg-white shadow-md py-4 border-b border-gray-200"></nav>
+        <nav className="bg-white shadow-md py-4 border-b border-gray-200">
+            {" "}
+            <div className="container mx-auto flex justify-between items-center px-6 lg:px-8">
+                <Link href={'/'}>
+                    <Image src={'/logo.jpg'} alt="logo" width={50} height={50}/>
+                    <span className="text-2xl font-bold text-gray-800">
+                        {" "}
+                    Travel Planner
+                    </span>
+                </Link>
+            </div>
+        </nav>
     );
 }
