cd6234008f82639d0435aadcf7555639620fe023
Diganta Singh Panwar <114343344+digantaa@users.noreply.github.com>
2025-10-25 19:51:37 +0530

    setting up authentication

--- auth.ts
+++ auth.ts
@@ -1,5 +1,9 @@
 import NextAuth from 'next-auth';
+import GitHub from "next-auth/providers/github";
+import {PrismaAdapter} from "@auth/prisma-adapter";
+import {prisma} from "@/lib/prisma"
 
-export const {} = NextAuth({
-    providers: [Git],
+export const {auth, handler, signIn, signOut } = NextAuth({
+    providers: [GitHub],
+    adapter: PrismaAdapter(prisma),
 })
